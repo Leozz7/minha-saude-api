0d787f567fea5c7a68041374fde8f5b1307f8bd9
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-09-05 10:44:20 -0300

    criação Service

--- src/main/java/com/tdeBack/minhaSaude/service/UsuarioService.java
+++ src/main/java/com/tdeBack/minhaSaude/service/UsuarioService.java
@@ -1,4 +1,45 @@
 package com.tdeBack.minhaSaude.service;
 
+import com.tdeBack.minhaSaude.model.Usuario;
+import com.tdeBack.minhaSaude.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario criarUsuario(Usuario usuario) {
+        if (usuarioRepository.existsByEmail(usuario.getEmail())) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario uAtualizado) {
+        Usuario u = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario nao encontrado"));
+
+        u.setNome(uAtualizado.getNome());
+        u.setEmail(uAtualizado.getEmail());
+        return usuarioRepository.save(u);
+    }
+
+    public void deletarUsuario(Long id) {
+        Usuario u = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario nao encontrado"));
+
+        usuarioRepository.delete(u);
+    }
+
+    public List<Usuario> listar() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarEmail(Usuario u) {
+        return usuarioRepository.findByEmail(u.getEmail());
+    }
 }
