70357acc1fbb2f7ad7f08239c018cba634c69bf2
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-09-16 11:45:17 -0300

    Mudando o tipo de retorno do /criar

--- src/main/java/com/tdeBack/minhaSaude/Config/Security/SecurityConfig.java
+++ src/main/java/com/tdeBack/minhaSaude/Config/Security/SecurityConfig.java
@@ -42,7 +42,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
