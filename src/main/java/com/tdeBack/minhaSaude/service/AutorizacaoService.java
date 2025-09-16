6caec180b0059816f930d4e2f7451add8c0f9ff2
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-09-16 11:44:15 -0300

    Evitando Recursividade

---
+++ src/main/java/com/tdeBack/minhaSaude/service/AutorizacaoService.java
@@ -0,0 +1,20 @@
+package com.tdeBack.minhaSaude.service;
+
+import com.tdeBack.minhaSaude.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorizacaoService implements UserDetailsService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email);
+    }
+}
