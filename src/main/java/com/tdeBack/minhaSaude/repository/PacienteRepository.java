ac0831f671b502deeb1dab9a4f7578fab63e296b
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-10-25 18:57:54 -0300

    Adicionando a pesquisa por nome no Reporitorio de Paciente

--- src/main/java/com/tdeBack/minhaSaude/repository/PacienteRepository.java
+++ src/main/java/com/tdeBack/minhaSaude/repository/PacienteRepository.java
@@ -10,4 +10,6 @@ public interface PacienteRepository extends JpaRepository<Paciente ,Long> {
 
     Paciente findByEmail(String email);
     boolean existsByEmail(String email);
+
+    Paciente findByName(String name);
 }
