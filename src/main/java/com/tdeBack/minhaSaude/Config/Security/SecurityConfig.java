7da41d2c8e2591d3571829bb95d1f9fdbb7d1537
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-01 21:44:12 -0300

    Adicionando o Swagger para exibir os endpoints da API

--- src/main/java/com/tdeBack/minhaSaude/Config/Security/SecurityConfig.java
+++ src/main/java/com/tdeBack/minhaSaude/Config/Security/SecurityConfig.java
@@ -35,12 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/procedimento/criar").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/procedimento/atualizar/{id}").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/procedimento/deletar/{id}").hasRole("ADMIN")
-                        .requestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-resources/**",
-                                "/swagger-resources",
-                                "/v3/api-docs.yaml"
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-resources", "/v3/api-docs.yaml"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
