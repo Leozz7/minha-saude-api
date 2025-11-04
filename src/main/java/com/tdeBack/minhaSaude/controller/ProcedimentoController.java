f265cba35ade2d74253fc3cc1a01fd66ecbdda89
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-04 09:17:57 -0300

    Adicionando o Controller de Procedimento

---
+++ src/main/java/com/tdeBack/minhaSaude/controller/ProcedimentoController.java
@@ -0,0 +1,49 @@
+package com.tdeBack.minhaSaude.controller;
+
+import com.tdeBack.minhaSaude.dto.ProcedimentoDTO;
+import com.tdeBack.minhaSaude.model.Procedimento;
+import com.tdeBack.minhaSaude.service.ProcedimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/procedimentos")
+public class ProcedimentoController {
+
+    @Autowired
+    private ProcedimentoService procedimentoService;
+
+    @PostMapping("/criar")
+    public ResponseEntity<Procedimento> criar(@RequestBody ProcedimentoDTO dto) {
+        Procedimento p = new Procedimento();
+        p.setNome(dto.getNome());
+        p.setDescricao(dto.getDescricao());
+        p.setValorPlano(dto.getValorPlano());
+        p.setValorParticular(dto.getValorParticular());
+        return ResponseEntity.ok(procedimentoService.criar(p));
+    }
+
+    @PutMapping("atualizar/{id}")
+    public ResponseEntity<Procedimento> atualizar(@PathVariable Long id, @RequestBody ProcedimentoDTO dto) {
+        Procedimento p = new Procedimento();
+        p.setNome(dto.getNome());
+        p.setDescricao(dto.getDescricao());
+        p.setValorPlano(dto.getValorPlano());
+        p.setValorParticular(dto.getValorParticular());
+        return ResponseEntity.ok(procedimentoService.atualizar(id, p));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Procedimento>> listar() {
+        return ResponseEntity.ok(procedimentoService.listar());
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        procedimentoService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
