df6ad80e5843ed4fecac2b3470434073e6ca161e
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-04 09:17:29 -0300

    Adicionando um DTO para Atendimento

---
+++ src/main/java/com/tdeBack/minhaSaude/dto/AtendimentoDTO.java
@@ -0,0 +1,19 @@
+package com.tdeBack.minhaSaude.dto;
+
+import com.tdeBack.minhaSaude.enums.TipoPagamento;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class AtendimentoDTO {
+    private Long id;
+    private Long usuarioId;
+    private Long pacienteId;
+    private String numeroCarteira;
+    private TipoPagamento tipoPagamento;
+    private Date dataAtendimento;
+    private Double valorTotal;
+    private List<Long> procedimentoIds;
+}
