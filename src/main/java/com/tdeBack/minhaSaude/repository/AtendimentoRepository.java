c6fac459603ba58a8a0d5321862ef97839f0d720
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-06 10:40:11 -0300

    Adicionando repositorio de Atendimento

---
+++ src/main/java/com/tdeBack/minhaSaude/repository/AtendimentoRepository.java
@@ -0,0 +1,7 @@
+package com.tdeBack.minhaSaude.repository;
+
+import com.tdeBack.minhaSaude.model.Atendimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtendimentoRepository extends JpaRepository<Atendimento, Long> {
+}
