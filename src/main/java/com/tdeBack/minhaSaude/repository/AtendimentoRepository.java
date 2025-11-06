ce84a18208a3e9e11f3c170ddd86985d399fa841
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-06 11:08:19 -0300

    Adicionando a busca de atendimento por id de paciente

--- src/main/java/com/tdeBack/minhaSaude/repository/AtendimentoRepository.java
+++ src/main/java/com/tdeBack/minhaSaude/repository/AtendimentoRepository.java
@@ -3,5 +3,8 @@ package com.tdeBack.minhaSaude.repository;
 import com.tdeBack.minhaSaude.model.Atendimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AtendimentoRepository extends JpaRepository<Atendimento, Long> {
+    List<Atendimento> findByPacienteId(Long PacienteId);
 }
