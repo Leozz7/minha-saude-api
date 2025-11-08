f80d4d353c699656780db092a457ef7ddb8d1c00
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-08 19:57:47 -0300

    Adicionando o dto de Paciente

---
+++ src/main/java/com/tdeBack/minhaSaude/dto/PacienteDTO.java
@@ -0,0 +1,19 @@
+package com.tdeBack.minhaSaude.dto;
+
+import com.tdeBack.minhaSaude.model.Responsavel;
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+public class PacienteDTO {
+    private String cpf;
+    private String email;
+    private String nome;
+    private String telefone;
+    private Date dataNascimento;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private Responsavel responsavel;
+}
