c86de62bef98925ddc3173341157179441685a89
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-11-13 12:37:19 -0300

    Adicionando o controller de Atendimento

--- src/main/java/com/tdeBack/minhaSaude/controller/AtendimentoController.java
+++ src/main/java/com/tdeBack/minhaSaude/controller/AtendimentoController.java
@@ -1,15 +1,22 @@
 package com.tdeBack.minhaSaude.controller;
 
-import com.sun.nio.sctp.IllegalReceiveException;
-import com.tdeBack.minhaSaude.dto.AtendimentoDTO;
-import com.tdeBack.minhaSaude.model.Atendimento;
-import com.tdeBack.minhaSaude.service.AtendimentoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.tdeBack.minhaSaude.dto.AtendimentoDTO;
+import com.tdeBack.minhaSaude.model.Atendimento;
+import com.tdeBack.minhaSaude.service.AtendimentoService;
 
 @RestController
 @RequestMapping("/api/atendimentos")
